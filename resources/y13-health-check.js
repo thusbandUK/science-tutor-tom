55ab1f377690f2bce957efd589189411f9029b65
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-09-22 18:41:20 +0100

    new form harvest function added

---
+++ resources/y13-health-check.js
@@ -0,0 +1,15 @@
+//var checkedBoxes = document.querySelectorAll('input[name=mycheckboxes]:checked');
+
+//from: https://stackoverflow.com/questions/8563240/how-to-get-all-checked-checkboxes
+
+/*
+Here's what this looks like, on submit, you need to have an array of questions 1 to 11
+
+[1,2,3,4,5,6,7,8,9,10,11]
+
+and then you need to do map entries, forEach, that sort of thing
+document.querySelectorAll('input[name=checkedBoxesQ${array[x]}])
+object.q1 = 
+
+
+*/
